1091759aa1be9a30c8b2a4215144337d7dccd542
Kulbhushan.Saxena <Kulbhushan.Saxena09@gmail.com>
2020-01-13 15:12:18 +0530

    Second Commit

--- springWebApplication/src/main/java/demo/spring/springWebApplication/model/Author.java
+++ springWebApplication/src/main/java/demo/spring/springWebApplication/model/Author.java
@@ -17,7 +17,8 @@ public class Author {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	private String name;
+	private String firstName;
+	private String lastName;
 	
 	@ManyToMany(mappedBy = "authors")
 	private Set<Book> books = new HashSet<Book>();
@@ -30,12 +31,20 @@ public class Author {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public Set<Book> getBooks() {
@@ -44,6 +53,36 @@ public class Author {
 
 	public void setBooks(Set<Book> books) {
 		this.books = books;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
 	}
 
 }
